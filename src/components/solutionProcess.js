e8da557403bfac3503f09af5b0f49a7f17d2320b
TuyizeeAnastase <tuyizereanastase1@gmail.com>
2021-04-21 21:41:50 +0200

    adding-solution-section

---
+++ src/components/solutionProcess.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+const Solution=()=>{
+    return (
+        <div>
+           <div className="font-semibold text-blue-500 text-4xl tracking-wider leading-relaxed mt-20 ml-24 mr-14 w-5/12">
+           Manual testing
+           </div>
+           <div className="text-blue-500 ml-96 center mt-8 mr-14 text-base mb-20 font-normal not-italic w-1/2">
+           <p>We take the role of the user and test your products in terms of functionality, design, and usability. 
+           We offer the following manual testing services for web, mobile, and desktop apps:
+           
+           · Test plan creation
+           · Functional testing
+           · Regression testing
+           · UI testing, cross-browser testing
+           · Usability and accessibility testing
+           </p>
+           </div>
+           <div className="font-semibold text-blue-500 text-4xl tracking-wider leading-relaxed mt-20 ml-24 mr-14 w-5/12">
+           Automated testing
+           </div>
+           <div className="text-blue-500 ml-96 center mt-8 mr-14 text-base mb-20 font-normal not-italic w-1/2">
+           <p>Save time and increase your testing coverage with automated testing.
+           We offer the following automated testing services for web, mobile, and desktop apps:
+           
+           · API testing
+           · Regression testing
+           · Ruby, Python
+           · Tool selection
+           · Review of existing automation
+           </p>
+           </div>
+           <div className="font-semibold text-blue-500 text-4xl tracking-wider leading-relaxed mt-20 ml-24 mr-14 w-5/12">
+           Performance testing
+           </div>
+           <div className="text-blue-500 ml-96 center mt-8 mr-14 text-base mb-20 font-normal not-italic w-1/2">
+           <p>
+           Ensure customer satisfaction by guaranteeing optimal performance during standard and peak workloads. 
+           We identify key performance criteria, provide you with information about speed, stability and scalability, 
+           and determine which areas need improvement.
+           </p>
+           </div>
+        </div>
+    )
+}
+
+export default Solution;
