718a5e492ba64851b8b5243bb91e2cf089aab95f
TuyizeeAnastase <tuyizereanastase1@gmail.com>
2021-04-21 22:23:57 +0200

    adding-impacts

---
+++ src/components/Impacts.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Impacts=()=>{
+    return (
+        <div>
+           <div className="font-semibold text-blue-500 text-4xl tracking-wider leading-relaxed mt-20 ml-24 mr-14 w-4/12">
+           We empower women to pursue careers in technology.
+           </div>
+           <div className="text-blue-500 ml-96 center mt-8 mr-4 text-base mb-20 font-normal not-italic w-1/2">
+           <p>In recent years, Rwanda has become well-known for championing women’s empowerment and advancing gender equality. 
+           Together with GIZ, Rwanda launched an education program to train women for jobs in tech. 
+           Once they have successfully completed the program, participants receive ISTQB® certification and are hired by Muraho Technology,
+            a Rwandan software testing company that partners with Betahills. By delivering world-class service on the international market, 
+            Muraho is key player in Rwanda’s transition into a tech hub. And by connecting the team at Muraho with European clients, 
+           Betahills is helping to empower women to pursue careers in Kigali’s booming IT and technology sector.
+           </p>
+           </div>
+        </div>
+    )
+}
+
+export default Impacts;
