751c42b2a07cf9cdb6446b872b521734e188a267
TuyizeeAnastase <tuyizereanastase1@gmail.com>
2021-04-21 22:30:35 +0200

    adding-contacts

---
+++ src/components/Contacts.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Contact=()=>{
+    return (
+        <div className="bg-green-300">
+      <div className="flex">
+        <div className="flex ml-24 mt-20 text-justify w-5/12 h-24 font-medium non-italic text-6xl leading-none text-blue-600">
+        Want to know more?
+        Get in touch. 
+        </div>
+        <button className="relative text-blue-600 border-2 border-blue-600 text-center mt-96 ml-32 mb-28 w-44 h-12 hover:text-gray-50 hover:bg-green-300">
+        info@betahills.com
+        </button>
+      </div>
+      </div>
+    )
+}
+
+export default Contact;
